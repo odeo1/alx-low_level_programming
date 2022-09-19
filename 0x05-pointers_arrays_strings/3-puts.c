c7d843fe2a490802cbc7036ea9c96d53df8475ef
odeo1 <sharonodeo@gmail.com>
2022-09-19 11:31:02 -0700

    print string followed by new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _put - prints string
+ * @str: string
+ * Return: void
+ */
+
+void _put(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
